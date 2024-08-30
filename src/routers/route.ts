233d0161a8d594e8c1c04cb9970b4cb90ba9ef9d
Milena Leme <milenaleme4@hotmail.com>
2024-08-30 17:08:58 -0300

    feat: insert post/patch and get

--- src/routers/route.ts
+++ src/routers/route.ts
@@ -1,9 +1,13 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
+import { measureController } from "../controllers/measureController";
+import schemaValidator from "../middleware/schemaValidator";
 
 const router: Router = Router();
 
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send("Hello Word")
-})
+router.post('/upload', schemaValidator('/auth/upload'), measureController.update)
+
+router.patch('/confirm', schemaValidator('/auth/confirm'), measureController.confirm)
+
+router.get('/:customer_code/list', measureController.list)
 
 export { router };
